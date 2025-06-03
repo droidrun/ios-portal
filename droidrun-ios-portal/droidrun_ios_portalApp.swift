fd5c555bce3126cb0f943bb9dc19698358451505
tbeck <beckmann.timo@icloud.com>
2025-06-03 12:59:56 +0200

    Initial Commit

---
+++ droidrun-ios-portal/droidrun_ios_portalApp.swift
@@ -0,0 +1,17 @@
+//
+//  droidrun_ios_portalApp.swift
+//  droidrun-ios-portal
+//
+//  Created by Timo Beckmann on 03.06.25.
+//
+
+import SwiftUI
+
+@main
+struct droidrun_ios_portalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
